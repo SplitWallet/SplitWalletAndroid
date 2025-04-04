1779a8842b3b22efd692fa71ea407c3f91cd5bb4
SkriptonX <stepanershov8@gmail.com>
2025-04-04 13:58:50 +0300

    expenses datetime fixed

--- app/src/main/java/com/example/splitwallet/api/RetrofitClient.java
+++ app/src/main/java/com/example/splitwallet/api/RetrofitClient.java
@@ -1,12 +1,22 @@
 package com.example.splitwallet.api;
 
 import android.annotation.SuppressLint;
+import android.util.Log;
 
+import com.example.splitwallet.models.LocalDateAdapter;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
 
+import java.lang.reflect.Type;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -17,13 +27,26 @@ public class RetrofitClient {
 
     public static Retrofit getRetrofitInstance() {
         if (retrofit == null) {
+            // Создаем интерцептор для логирования
+            HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor(message ->
+                    Log.d("RETROFIT_HTTP", message)
+            );
+            loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+            // Настраиваем OkHttp клиент
+            OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                    .addInterceptor(loggingInterceptor)
+                    .build();
+
             Gson gson = new GsonBuilder()
-                    .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeDeserializer())
+                    .registerTypeAdapter(LocalDate.class, new LocalDateAdapter()) // Для даты
+                    .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeDeserializer()) // Для даты-времени
                     .create();
 
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
-                    .addConverterFactory(GsonConverterFactory.create(gson)) // Используем наш кастомный Gson
+                    .client(okHttpClient) // Добавляем наш клиент с логированием
+                    .addConverterFactory(GsonConverterFactory.create(gson))
                     .build();
         }
         return retrofit;
