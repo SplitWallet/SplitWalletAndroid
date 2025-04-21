410aabfcba73b09f7a8e59ec4559c27678adcbbc
Olesya <114682776+Olesya-Kosminskaya@users.noreply.github.com>
2025-04-22 00:04:54 +0300

    added group deletion

--- app/src/main/java/com/example/splitwallet/api/ApiService.java
+++ app/src/main/java/com/example/splitwallet/api/ApiService.java
@@ -8,6 +8,7 @@ import com.example.splitwallet.models.Group;
 import com.example.splitwallet.models.JWTtoken;
 import com.example.splitwallet.models.LoginRequest;
 import com.example.splitwallet.models.RegisterRequest;
+import com.example.splitwallet.models.UpdateExpenseRequest;
 import com.example.splitwallet.models.User;
 import com.example.splitwallet.models.UserResponse;
 
@@ -16,11 +17,14 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
+
 public interface ApiService {
     @POST("login")
     Call<JWTtoken> login(@Body LoginRequest loginRequest);
@@ -40,6 +44,7 @@ public interface ApiService {
             @Header("Authorization") String authToken,
             @Body CreateExpenseRequest request
     );
+
     @GET("groups/my")
     Call<List<Group>> getUserGroups(
             @Header("Authorization") String authToken
@@ -51,8 +56,23 @@ public interface ApiService {
             @Header("Authorization") String authToken
     );
 
+    @PUT("groups/{groupId}/expenses/{expenseId}")
+    Call<Expense> updateExpense(
+            @Path("groupId") Long groupId,
+            @Path("expenseId") Long expenseId,
+            @Header("Authorization") String authToken,
+            @Body UpdateExpenseRequest request
+    );
+
+    @DELETE("groups/{groupId}/expenses/{expenseId}")
+    Call<Void> deleteExpense(
+            @Path("groupId") Long groupId,
+            @Path("expenseId") Long expenseId,
+            @Header("Authorization") String authToken
+    );
+
     @GET("groups/{groupId}/members")
-    Call<List<UserResponse>> getGroupMembers(
+    Call<List<User>> getGroupMembers(
             @Path("groupId") Long groupId,
             @Header("Authorization") String authToken
     );
@@ -62,4 +82,11 @@ public interface ApiService {
             @Path("groupId") Long groupId,
             @Header("Authorization") String authToken
     );
+
+    @DELETE("groups/{id}")
+    Call<Void> deleteGroup(
+            @Header("Authorization") String token,
+            @Path("id") Long groupId
+    );
+
 }
