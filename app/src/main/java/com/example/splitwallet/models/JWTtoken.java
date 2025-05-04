d97af47ff9898f4c9710024eae88a101f3169440
Olesya <114682776+Olesya-Kosminskaya@users.noreply.github.com>
2025-05-04 10:29:37 +0300

    add fields and methods for tests

--- app/src/main/java/com/example/splitwallet/models/JWTtoken.java
+++ app/src/main/java/com/example/splitwallet/models/JWTtoken.java
@@ -1,11 +1,13 @@
 package com.example.splitwallet.models;
 
+import com.google.gson.annotations.SerializedName;
+
 import lombok.Getter;
 
 @Getter
 public class JWTtoken {
     private String jwtToken;
-
+    @SerializedName("token") // Для тестов
     public String getJwtToken()
     {
         return this.jwtToken;
